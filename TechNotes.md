c03531175d2cc458ea97df652eac0fde916860bd
Mohammad <mohammad.khlouf@gmail.com>
2026-04-14 22:07:23 +0300

    create first table and migration

---
+++ TechNotes.md
@@ -0,0 +1,4 @@
+##supabse:
+you have to create 2 clients one for server the other for client.
+client: for the browser or client side.
+server for server components, server actions, route handlers.
---
+++ supabase/migrations/20260414172937_create_users.sql
@@ -0,0 +1,7 @@
+create table public.users(
+    id uuid primary key default gen_random_uuid(),
+    email text not null unique,
+    password text not null,
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
+)
