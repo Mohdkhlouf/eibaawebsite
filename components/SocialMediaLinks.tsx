a3b9e480472acce9b0d4f0f19b85204f3ee7a963
Mohammad <mohammad.khlouf@gmail.com>
2026-04-23 15:33:41 +0300

    adding react-icons to social media icons

--- components/SocialMediaLinks.tsx
+++ components/SocialMediaLinks.tsx
@@ -1,26 +1,29 @@
 "use client"
 import React from 'react'
+import { FaFacebook, FaInstagram, FaTelegram, FaWhatsapp, FaTiktok} from 'react-icons/fa'
+import { IconType } from 'react-icons'
 
 type SocialLink = {
   href: string
-  icon: string
+  icon: IconType
 }
 
 const links: SocialLink[] = [
-  { href: 'https://www.facebook.com/eibaa.abutaha', icon: 'fa-brands fa-facebook' },
-  { href: 'https://www.instagram.com/eiba_abutaha', icon: 'fa-brands fa-instagram' },
-  { href: 'https://t.me/eibaabutaha', icon: 'fa-brands fa-telegram' },
-  { href: 'https://wa.me/+972598183137', icon: 'fa-brands fa-whatsapp' },
-  { href: 'https://www.tiktok.com/@eiba_abutaha', icon: 'fa-brands fa-tiktok' },
-  { href: 'https://open.spotify.com/show/7nDHB7lyMd1GBwz5pxapzy', icon: 'fa-brands fa-spotify' },
+  { href: 'https://www.facebook.com/eibaa.abutaha', icon: FaFacebook },
+  { href: 'https://www.instagram.com/eiba_abutaha', icon: FaInstagram },
+  { href: 'https://t.me/eibaabutaha', icon: FaTelegram },
+  { href: 'https://wa.me/+972598183137', icon: FaWhatsapp },
+  { href: 'https://www.tiktok.com/@eiba_abutaha', icon: FaTiktok },
 ]
 
 export const SocialMediaLinks: React.FC = () => {
   return (
     <div className="flex items-center justify-center gap-2">
-      {links.map(({ href, icon }) => (
+      {links.map(({ href, icon: Icon }) => (
         <a key={href} href={href} target="_blank" rel="noopener noreferrer" aria-label={`Open ${href}`}>
-          <i className={`${icon} rounded-full w-8 h-8 flex items-center justify-center bg-white text-[var(--mainColor)] text-sm leading-8 m-2 text-center`} aria-hidden="true" />
+          <span className="rounded-full w-8 h-8 flex items-center justify-center bg-white text-[var(--mainColor)] text-sm m-2">
+            <Icon />
+          </span>
         </a>
       ))}
     </div>
