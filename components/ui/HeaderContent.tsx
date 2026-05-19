1b1984bec879b7083a71986c542c0e7c6a6c52af
Mohammad <mohammad.khlouf@gmail.com>
2026-05-19 12:32:02 +0300

    fix main header align text:

--- components/ui/HeaderContent.tsx
+++ components/ui/HeaderContent.tsx
@@ -7,9 +7,9 @@ type SocialLink = { id: number; url: string; icon: string; name: string }
 export const HeaderContent = ({ socialLinks }: { socialLinks: SocialLink[] }) => {
   const scrollRef = useRef(null)
   return (
-    <section className="bg-[linear-gradient(to_right,rgba(61,51,80,0.85),rgba(124,107,138,0.5)),url(/background.png)] bg-cover bg-center bg-no-repeat min-h-125">
-      <div className="max-w-250 min-h-125 mx-auto">
-        <div className="flex flex-col md:items-end items-center text-white py-8" ref={scrollRef}>
+    <section className="flex flex-col  bg-[linear-gradient(to_right,rgba(61,51,80,0.85),rgba(124,107,138,0.5)),url(/background.png)] bg-cover bg-center bg-no-repeat min-h-125">
+      <div className="flex flex-col flex-1 align-middle max-w-250 min-h-125 mx-auto">
+        <div className="flex flex-col  justify-center flex-1  md:items-end items-center text-white py-8" ref={scrollRef}>
           <div className="w-3/5 flex flex-col items-center justify-center">
             <h2 className="text-2xl font-bold leading-12 drop-shadow-lg text-center">
               هناك أوقات نتعثر فيها ونحتاج أن نجلب الشمس للعلاقة
